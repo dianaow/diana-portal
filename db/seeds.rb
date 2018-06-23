f4eeffb8bda4ab8bc3e7f2f465daabb792429422
Diana <diana.ow@gmail.com>
2018-06-23 14:25:30 +0000

    restyled landing page and article show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,43 +11,6 @@ end
 
 puts "20 Users created"
 
-category_array = []                                                         
-10.times do                                                      
-  category_array << Category.create!(name: Faker::ProgrammingLanguage.unique.name)                 
-end 
-
-puts "10 Categories created"
-
-user_array.each do |user|
-    user_array.length.times do |n|
-      Article.create!(title: Faker::Lorem.unique.sentence.chomp('.'),
-               summary: Faker::Lorem.unique.sentence(10, false, 10),
-               description: Faker::Lorem.paragraph(rand(3..100)),
-               user_id: user.id,
-               status: 0,
-               impressions_count: 30-n,
-               cached_votes_score: 30-n)
-    end
-end
-
-puts "200 Articles created"
-
-
-20.times do |n|
-  ArticleCategory.create!(article_id: n+1, 
-                      category_id: 10)
-end
-20.times do |n|
-  ArticleCategory.create!(article_id: n+21, 
-                      category_id: 9)
-end
-9.times do |n|
-  ArticleCategory.create!(article_id: n+41, 
-                      category_id: n+1)
-end
-
-puts "50 categorized articles"
-
 
 8.times do |n| 
   Friendship.create!(user_id: 20-n, friend_id: n+1, accepted: true)
--- spec/features/vote_for_article_spec.rb
+++ spec/features/vote_for_article_spec.rb
@@ -12,11 +12,11 @@ describe 'vote', js: true do
     
     it 'has an icon for user to like an article' do
       expect(page).to have_link("", href: "/articles/#{article.id}/upvote")
-      expect(page).to have_css("#article-sidebar", text: "0 likes")
+      expect(page).to have_css("#small-article-sidebar", text: "0 likes")
     end
     
     it 'allows user to like an article' do
-      within "#article-sidebar" do
+      within "#small-article-sidebar" do
         find("a[href = '/articles/#{article.id}/upvote']").click
         expect(page).to have_content("1 like")
         expect(page).to have_link("", href: "/articles/#{article.id}/downvote")
@@ -24,7 +24,7 @@ describe 'vote', js: true do
     end
     
     it 'allows user to unlike an article, user can only like once' do
-      within "#article-sidebar" do
+      within "#small-article-sidebar" do
         find("a[href = '/articles/#{article.id}/upvote']").click
         expect(page).to have_content("1 like")
         find("a[href = '/articles/#{article.id}/downvote']").click
